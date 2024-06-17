0e276745b4c6ce9fc9d08d6e4327c109f86ce54b
gabrielteiga <gabrielteiga99@hotmail.com>
2024-06-17 19:46:49 -0300

    Projeto inicial

---
+++ src/main/java/com/grupo9/microsservicos_assinaturas/MicrosservicosAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo9.microsservicos_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicosAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicosAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo9/microsservicos_assinaturas/MicrosservicosAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.microsservicos_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicosAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
