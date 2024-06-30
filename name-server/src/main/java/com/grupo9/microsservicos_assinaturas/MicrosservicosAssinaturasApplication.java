f22149c1315498df6a5c12d8bfd0317c6ba1fa43
gabrielteiga <gabrielteiga99@gmail.com>
2024-06-29 23:01:47 -0300

    Criação do gateway

--- name-server/src/main/java/com/grupo9/microsservicos_assinaturas/MicrosservicosAssinaturasApplication.java
+++ name-server/src/main/java/com/grupo9/microsservicos_assinaturas/MicrosservicosAssinaturasApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class MicrosservicosAssinaturasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrosservicosAssinaturasApplication.class, args);
 	}
-
 }
